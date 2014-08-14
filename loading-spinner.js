d2a8eb43de0fd8166aa5db070e6cbbab3a6623dd
André Gustavo Espeiorin <andre.espeiorin@gmail.com>
2014-08-14 14:21:54 -0300

    Bug fixes

--- loading-spinner.js
+++ loading-spinner.js
@@ -1,42 +1,35 @@
 angular.module('loading-spinner', []).directive('loadingSpinner', function(){
 	return {
-		scope : {
-			loadingSpinner : "="
-		},
-		compile : function(element, attributes) {
+		restrict: 'A',
+        scope: true,
+        replace : false,
+		link : function(scope, element, attributes)
+		{
 			var loader = $('<span class="request-spin"><i class="fa fa-circle-o-notch fa-spin"></i></span>');
-			return function(scope, element, attributes)
-			{
-				scope.$watch('loadingSpinner', function(newValue, oldValue){
-					if (newValue) {
-						element.append(loader);
-					} else {
-						$(loader).remove();
-					}
-				}, true);
-			};
-		},
-		restricted : 'AE',
-		replace : false
+			scope.$watch(attributes['loadingSpinner'], function(newValue, oldValue){
+				if (newValue) {
+					element.append(loader);
+				} else {
+					$(loader).remove();
+				}
+			}, true);
+		}
 	};
 }).directive('overlaySpinner', function(){
 	return {
-		scope : {
-			overlaySpinner : '='
-		},
-		compile : function(element, attributes) {
-			var overlay = $('<div class="overlay-spinner"><span class="request-spin"><i class="fa fa-circle-o-notch fa-spin"></i><span></div>');
-			return function(scope, element, attributes){
-				scope.$watch('overlaySpinner', function(newValue, oldValue){
-					if (newValue) {
-						element.append(overlay);
-					} else {
-						$(overlay).remove();
-					}
-				}, true);
-			};
-		},
-		restricted : 'A',
-		replace : false
+		restrict: 'A',
+        scope: true,
+        replace : false,
+        link : function(scope, element, attributes)
+        {
+        	var overlay = $('<div class="overlay-spinner"><span class="request-spin"><i class="fa fa-circle-o-notch fa-spin"></i><span></div>');
+			scope.$watch(attributes['overlaySpinner'], function(newValue, oldValue){
+				if (newValue) {
+					element.append(overlay);
+				} else {
+					$(overlay).remove();
+				}
+			}, true);
+        }
 	}
 });
